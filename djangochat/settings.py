79b365ed12933e6a6ac1513f2ff9d9e8a2348155
peter1998 <pmatov@gmail.com>
2024-09-07 17:16:36 +0300

    Core App

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangochat.wsgi.application'
+ASGI_APPLICATION = 'djangochat.asgi.application'
 
 
 # Database
